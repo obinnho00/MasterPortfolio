fde2f3f7074602897e6f0ccd756d21d188b2a66c
obi <arumi21@students.ecu.edu>
2024-12-12 18:42:24 -0500

    changed setting

--- ProFileHub/ProFileHub/settings.py
+++ ProFileHub/ProFileHub/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from PortfolioApp.gitconfig.gitconfig import secrite_key
+from PortfolioApp.gitconfig.gitconfig import set
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrite_key
+school = set
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
